bb848e7150f551cb42a3ed555f3d309b7a8b8fa9
sourav <sourabhkatoch13@gmail.com>
2021-10-07 01:21:37 +0530

    integrating amazon SQS

--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -1,13 +1,13 @@
 import createError from 'http-errors'
 import {getDueAuctions} from '../lib/getDueAuctions'
-import {closeAuctions} from '../lib/closeAuctions'
+import {closeAuction} from '../lib/closeAuctions'
 
 async function processAuctions(event, context){
   try{
     let auctions = await getDueAuctions(new Date());
     // process auction for closing it
     let result;
-    result = auctions.map( auction => closeAuctions(auction));
+    result = auctions.map( auction => closeAuction(auction));
     await Promise.all(result);
     return {
         closed: result.length
--- src/lib/closeAuctions.js
+++ src/lib/closeAuctions.js
@@ -3,7 +3,7 @@ import AWS from "aws-sdk";
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 const sqs = new AWS.SQS();
 
-export async function closeAuctions(auction){
+export async function closeAuction(auction){
   const params = {
     TableName: process.env.AUCTION_TABLE_NAME,
     Key: {id:auction.id } ,
@@ -21,6 +21,18 @@ export async function closeAuctions(auction){
   
   const {bidder, creator, title, highestBid} = auction;
   const { amount } = highestBid
+
+  if (amount == 0){
+    await sqs.sendMessage({
+      QueueUrl: process.env.MAIL_QUEUE_URL,
+      MessageBody: JSON.stringify({
+          subject: "Your Item has no bids",
+          recipient: creator,
+          body: `Your item ${title} did not get any bids. Better Luck Next Time`
+      }),
+    }).promise();
+    return;
+  }
 
   const notifySeller = sqs.sendMessage({
     QueueUrl: process.env.MAIL_QUEUE_URL,
